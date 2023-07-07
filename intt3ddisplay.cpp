dc3795c541b1713112d89cfa0bed2712edb35649
alessio.illari@uconn.edu <aillari@sphnxdev01.sdcc.bnl.gov>
2023-07-07 11:32:54 -0400

    First commit

---
+++ intt3ddisplay.cpp
@@ -0,0 +1,17 @@
+#include "TFile.h"
+#include "TTree.h"
+#include "TBranch.h"
+#include "TGraph.h"
+#include "TGraph3D.h"
+#include "TLegend.h"
+#include "TStyle.h"
+
+void plotEvents()
+{
+
+}
+
+void runMacro()
+{
+	plotEvents();
+}
